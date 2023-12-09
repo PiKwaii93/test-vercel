4bca4dfd03476760fa66be367df37ea2814676da
maxence.nho <maxence.nho@hetic.net>
2023-12-09 12:10:43 +0100

    test 3

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ const home = require("./routes/home");
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+// app Use
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const port = process.env.PORT || 4000;
+
+require("express-async-errors");
+const io = require("socket.io")(4080, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+/* const mysqlPool = require("./database/config"); */
+
+// Middlewares
+/* const usersRoutes = require("./controllers/users.controller");
+const conversationsRoutes = require("./controllers/conversations.controller");
+const messagesRoutes = require("./controllers/messages.controller"); */
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 // Routes
 app.use("/home", home);
 
-// connection
-const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
